e943d6b7f153f2361443b2f215f11f8062e50f68
Hans Schuell <hans.schuell@giraone.com>
2022-11-05 22:38:16 +0100

    Check port

--- src/main/java/com/giraone/sb3/demo/ServiceApplication.java
+++ src/main/java/com/giraone/sb3/demo/ServiceApplication.java
@@ -1,18 +1,23 @@
 package com.giraone.sb3.demo;
 
 import com.giraone.sb3.demo.service.CalculationWebClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
-public class ServiceApplication1 {
+public class ServiceApplication {
+
+    @Autowired
+    private ServerProperties serverProperties;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication1.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 
     @Bean
@@ -22,7 +27,8 @@ public class ServiceApplication1 {
 
     @Bean
     HttpServiceProxyFactory httpServiceProxyFactory(WebClient.Builder builder) {
-        var wc = builder.baseUrl("http://localhost:8080");
-        return WebClientAdapter.createHttpServiceProxyFactory(wc);
+        final int port = serverProperties.getPort() != null ? serverProperties.getPort() : 8080;
+        WebClient.Builder webClientBuilder = builder.baseUrl("http://localhost:" + port);
+        return WebClientAdapter.createHttpServiceProxyFactory(webClientBuilder);
     }
 }
--- src/test/java/com/giraone/sb3/demo/ServiceApplicationTests.java
+++ src/test/java/com/giraone/sb3/demo/ServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceApplication1Tests {
+class ServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
