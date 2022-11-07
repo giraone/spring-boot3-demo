9afdf911c507c0d4f4914cd294fff50104c8051e
Hans Schuell <hans.schuell@giraone.com>
2022-11-07 13:36:23 +0100

    RequestLoggingFilterConfig added

---
+++ src/main/java/com/giraone/sb3/demo/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,19 @@
+package com.giraone.sb3.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setMaxPayloadLength(1000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
