4eaef2c4b973e9b369582cbfb1c1a50c4cef3133
Hans Schuell <hans.schuell@giraone.com>
2022-11-09 21:31:34 +0100

    New manual start scrips added

---
+++ start-demo1-with-otel-javaagent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SERVER_PORT=8080
+export APPLICATION_CLIENT_PORT=8081
+export SPRING_APPLICATION_NAME=demo1
+export OTEL_SERVICE_NAME=demo1
+
+export OTEL_METRICS_EXPORTER=otlp
+export OTEL_TRACES_EXPORTER=otlp
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
+export SPRING_PROFILES_ACTIVE=setup1
+
+java -javaagent:./observability/setup2/opentelemetry-javaagent.jar \
+     -jar target/demo-0.0.1-SNAPSHOT.jar
--- start-with-otel-javaagent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export OTEL_METRICS_EXPORTER=otlp
-java -javaagent:./observability/setup2/opentelemetry-javaagent.jar \
-     -jar target/demo-0.0.1-SNAPSHOT.jar
