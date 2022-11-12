e2c31ba05a89cb4f347cec71f176bdc4211dfc33
Hans Schuell <hans.schuell@giraone.com>
2022-11-12 21:04:16 +0100

    Setup for running the Spring Boot apps locally

--- start-demo1.sh
+++ start-demo1.sh
@@ -7,12 +7,11 @@ export OTEL_SERVICE_NAME=demo1
 
 export SPRING_PROFILES_ACTIVE=setup1
 
-export OTEL_METRICS_EXPORTER=otlp
-export OTEL_TRACES_EXPORTER=otlp
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
+export OTEL_METRICS_EXPORTER=none
+export OTEL_TRACES_EXPORTER=zipkin
+export OTEL_EXPORTER_TRACES_ENDPOINT=http://localhost:9411/api/v2/spans # zipkin ingest
 
-# export OTEL_PROPAGATORS=b3
+export OTEL_PROPAGATORS=b3
 
 java \
-  -javaagent:./observability/setup2/opentelemetry-javaagent.jar \
   -jar target/demo-0.0.1-SNAPSHOT.jar
