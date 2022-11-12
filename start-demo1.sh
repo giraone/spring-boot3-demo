3ebb8609cab066b38a561f2bdd8771914233175c
Hans Schuell <hans.schuell@giraone.com>
2022-11-12 21:45:19 +0100

    OTEL_PROPAGATORS/OTEL_TRACES_SAMPLER added

--- start-demo1.sh
+++ start-demo1.sh
@@ -11,7 +11,7 @@ export OTEL_METRICS_EXPORTER=none
 export OTEL_TRACES_EXPORTER=zipkin
 export OTEL_EXPORTER_TRACES_ENDPOINT=http://localhost:9411/api/v2/spans # zipkin ingest
 
-export OTEL_PROPAGATORS=b3
+export OTEL_PROPAGATORS=tracecontext,baggage,b3,b3multi
+export OTEL_TRACES_SAMPLER=always_on
 
-java \
-  -jar target/demo-0.0.1-SNAPSHOT.jar
+java -jar target/demo-0.0.1-SNAPSHOT.jar
