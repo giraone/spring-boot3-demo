0d7e3d5fc33242eb75a588f4fed89dd6a14432b9
Hans Schuell <hans.schuell@gmx.de>
2022-11-18 16:31:55 +0100

    Startup log improved

--- src/main/java/com/giraone/sb3/demo/ServiceApplication.java
+++ src/main/java/com/giraone/sb3/demo/ServiceApplication.java
@@ -11,11 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -29,7 +33,9 @@ public class ServiceApplication {
     }
 
     public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication app = new SpringApplication(ServiceApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
 	}
 
     @Bean
@@ -65,5 +71,49 @@ public class ServiceApplication {
         OpenTelemetrySdk openTelemetrySdk = AutoConfiguredOpenTelemetrySdk.initialize()
             .getOpenTelemetrySdk();
         LOGGER.info("{}", openTelemetrySdk);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (!StringUtils.hasText(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        LOGGER.info("""
+                ----------------------------------------------------------
+                ~~~ Application '{}' is running! Access URLs:
+                ~~~ - Local:      {}://localhost:{}{}
+                ~~~ - External:   {}://{}:{}{}
+                ~~~ Java version:      {} / {}
+                ~~~ Processors:        {}
+                ~~~ Profile(s):        {}
+                ~~~ Default charset:   {}
+                ~~~ File encoding:     {}
+                ----------------------------------------------------------""",
+            env.getProperty("spring.application.name"),
+            protocol,
+            serverPort,
+            contextPath,
+            protocol,
+            hostAddress,
+            serverPort,
+            contextPath,
+            System.getProperty("java.version"), System.getProperty("java.vm.name"),
+            Runtime.getRuntime().availableProcessors(),
+            env.getActiveProfiles(),
+            Charset.defaultCharset().displayName(),
+            System.getProperty("file.encoding")
+        );
     }
 }
