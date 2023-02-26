f60742721014d6379700414df6a49d6bd7bd519b
Hans Schuell <hans.schuell@gmx.de>
2023-02-27 00:13:10 +0100

    scripts with sample REST calls added

---
+++ call-endpoints.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+curl http://localhost:8080/calculate/10
+echo
+curl http://localhost:8080/calculate/20
+echo
+
+curl http://localhost:8080/calculate2/10
+echo
+curl http://localhost:8080/calculate2/20
+echo
+
+curl http://localhost:8080/sleep/100
+echo
+curl http://localhost:8080/sleep/1000
+echo
+
