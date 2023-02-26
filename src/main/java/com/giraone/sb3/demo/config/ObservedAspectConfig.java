5cd4c0284d6e2a53badcfae77e684a5361d0d925
Hans Schuell <hans.schuell@gmx.de>
2023-02-26 23:43:18 +0100

    ObservedAspectConfig added

---
+++ src/main/java/com/giraone/sb3/demo/config/ObservedAspectConfig.java
@@ -0,0 +1,14 @@
+package com.giraone.sb3.demo.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class ObservedAspectConfig {
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
