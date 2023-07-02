3e2c3433c92a678fc12b6fb882c68c3b4750d3a6
Hans Schuell <hans.schuell@gmx.de>
2023-07-02 11:14:11 +0200

    rearrange

--- start-demo1.sh
+++ start-demo1.sh
@@ -3,10 +3,9 @@
 export SERVER_PORT=8080
 export APPLICATION_CLIENT_PORT=8081
 export SPRING_APPLICATION_NAME=demo1
-export OTEL_SERVICE_NAME=demo1
-
 export SPRING_PROFILES_ACTIVE=setup1
 
+export OTEL_SERVICE_NAME=demo1
 export OTEL_METRICS_EXPORTER=none
 export OTEL_TRACES_EXPORTER=zipkin
 export OTEL_EXPORTER_TRACES_ENDPOINT=http://localhost:9411/api/v2/spans # zipkin ingest
