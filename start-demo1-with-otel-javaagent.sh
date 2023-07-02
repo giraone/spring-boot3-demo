13cb17141057f3b1afd9116f01fc1e403695d134
Hans Schuell <hans.schuell@gmx.de>
2023-07-02 11:32:37 +0200

    Using javaagent from target

--- start-demo1-with-otel-javaagent.sh
+++ start-demo1-with-otel-javaagent.sh
@@ -3,16 +3,30 @@
 export SERVER_PORT=8080
 export APPLICATION_CLIENT_PORT=8081
 export SPRING_APPLICATION_NAME=demo1
-export OTEL_SERVICE_NAME=demo1
+export SPRING_PROFILES_ACTIVE=setup2
 
-export SPRING_PROFILES_ACTIVE=setup1
+export OTEL_JAVA_AGENT_VERSION=1.27.0
+export OTEL_SERVICE_NAME=demo1
+export OTEL_RESOURCE_ATTRIBUTES="deployment.environment=production,service.namespace=sb3,service.version=1.1,service.instance.id=1"
 
 export OTEL_METRICS_EXPORTER=otlp
-export OTEL_TRACES_EXPORTER=otlp
+export OTEL_TRACES_EXPORTER=none
+export OTEL_LOGS_EXPORTER=none
+
+# this setting is for using an OTEL collector running locally
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
+
+# these 2 vars and 3 settings are for grafana cloud
+export GRAFANA_INSTANCE_ID="9999999"
+export GRAFANA_API_KEY="the-token-goes-here"
+#export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
+#export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp-gateway-prod-eu-west-2.grafana.net/otlp'
+#export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Basic $(echo -n $GRAFANA_INSTANCE_ID:$GRAFANA_API_KEY | base64 -w 0)"
+
+echo $OTEL_EXPORTER_OTLP_HEADERS
 
 # export OTEL_PROPAGATORS=b3
 
 java \
-  -javaagent:./observability/setup2/opentelemetry-javaagent.jar \
+  -javaagent:./target/opentelemetry-javaagent-${OTEL_JAVA_AGENT_VERSION}.jar \
   -jar target/demo-sb3.jar
